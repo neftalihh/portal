07c65cb00da58dac3b60f25d362638fa3ab43d0a
Daniel Martinez <danielmartinez@MacBook-Pro-de-Daniel.local>
2018-05-14 11:26:37 -0500

    Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+declare var jQuery:any;
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+
+  ngAfterViewInit(){
+    $(document).ready(function(){
+      $(".dropdown-trigger").dropdown();
+    });
+  }
 }
